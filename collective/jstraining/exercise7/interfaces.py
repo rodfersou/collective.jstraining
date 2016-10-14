6bc7416ad22d754fea8b446576a84eeaa6d361a1
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-10-14 14:25:01 -0500

    add stub files for exercises

---
+++ collective/jstraining/exercise7/interfaces.py
@@ -0,0 +1,10 @@
+from plone.formwidget.namedfile.interfaces import INamedImageWidget
+from zope.interface import Interface
+
+
+class IMinSizeImageWidget(INamedImageWidget):
+    pass
+
+
+class IExercise7Layer(Interface):
+    pass
